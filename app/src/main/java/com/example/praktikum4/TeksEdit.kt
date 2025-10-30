f91f9b305228fe22eaf962baae318befc0e610c5
maskikuy <kproject305@gmail.com>
2025-10-30 11:42:33 +0700

    menambahkan variabel untuk menyimpan data yang diperoleh dari komponen UI

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -1,18 +1,26 @@
 package com.example.praktikum4
 
+import android.R
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import java.lang.reflect.Modifier
 
 
 @Composable
-fun FormDataDiri(modifier: Modifier)
+fun FormDataDiri(modifier: Modifier
 ){
-    var textNama by remember { mutableStateOf(value = "") }
-    var textAlamat by remember { mutableStateOf(value = "") }
-    var textJk by remember { mutableStateOf(value = "") }
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJk by remember { mutableStateOf("") }
 
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf( "") }
+    var jenis by remember { mutableStateOf("") }
+
+    val gender: List<R.string> = listOf("Laki-laki","Perempuan")
 
 
 }
