196739fc602a763f6590090eb50032c990b8c126
maskikuy <kproject305@gmail.com>
2025-10-30 11:36:56 +0700

    menambahkan variabek untuk memngingat nilai masukan dr keyboard

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -1,11 +1,18 @@
 package com.example.praktikum4
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import java.lang.reflect.Modifier
 
 
 @Composable
 fun FormDataDiri(modifier: Modifier)
 ){
+    var textNama by remember { mutableStateOf(value = "") }
+    var textAlamat by remember { mutableStateOf(value = "") }
+    var textJk by remember { mutableStateOf(value = "") }
+
+
 
 }
