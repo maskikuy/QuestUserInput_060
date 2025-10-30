be4a38379d5eb35210fd57eef372f9e0780bc243
maskikuy <kproject305@gmail.com>
2025-10-30 11:57:35 +0700

    menambahkan variabel vertical

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -8,8 +8,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -49,9 +51,20 @@ fun FormDataDiri(modifier: Modifier
             }
         )
         Row {
-            gender.forEach { item -> }
+            gender.forEach { item ->
+                Row (modifier = Modifier.selectable(
+                    selected = textJk == item,
+                    onClick = { textJk = item}
+                ), verticalAlignment = Alignment.CenterVertically){
+                    RadioButton(
+                        selected = textJk == item,
+                        onClick = {
+                            textJk = item
+                        })
+                    Text(item)
+                }
+            }
         }
-        )
     }
 
 
