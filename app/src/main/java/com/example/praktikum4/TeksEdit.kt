857a48582114177ba0e02537e58baf5915bc8742
maskikuy <kproject305@gmail.com>
2025-10-30 12:22:26 +0700

    menambahkan string submit

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 
@@ -94,7 +95,11 @@ fun FormDataDiri(modifier: Modifier
                 jenis=textJk
                 alamat=textAlamat
             }
-        ) { }
+        ) {
+            Text(stringResource(R.string.submit))
+        }
+
+
     }
 
 
